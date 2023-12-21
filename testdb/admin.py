e229e17dd2902a8201b717828f8ef37cde2c6660
Kaylee <kayleespindler@gmail.com>
2023-12-20 18:15:09 -0600

    fix

---
+++ testdb/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from testdb.models import Testing
+
+admin.site.register(Testing)
+
+# Register your models here.
---
+++ testdb/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Testing(models.Model):
+    name = models.CharField(max_length=80)
+    number = models.IntegerField()
