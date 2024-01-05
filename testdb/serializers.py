1acef95b1572aef2cbd70513b9ffc7253a95716b
Kaylee <kayleespindler@gmail.com>
2024-01-05 00:18:58 -0600

    tweaked views.py

--- testdb/serializers.py
+++ testdb/serializers.py
@@ -5,4 +5,4 @@ from . models import Testing
 class TestSerializers(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta: 
         model = Testing # tell django which model to use
-        fields = ['name', 'number'] # tell django which fields to include
+        fields = ['id','name', 'number'] # tell django which fields to include
