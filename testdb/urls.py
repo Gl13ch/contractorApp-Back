3b87b898a0622255c327fc57bb7921491cbbf358
Kaylee <kayleespindler@gmail.com>
2024-01-05 00:26:08 -0600

    tweaked urls.py

--- testdb/urls.py
+++ testdb/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('testing/', views.TestingList.as_view(), name='testings_list'), # testing/ will be routed to the TestingList view for handling
-    path('testing/<int:pk>', views.TestingList.as_view(), name='testing_detail'), # api/contacts will be routed to the ContactDetail view for handling
+    path('testing/<int:pk>', views.TestingDetail.as_view(), name='testing_detail'), # api/contacts will be routed to the ContactDetail view for handling
 ]
