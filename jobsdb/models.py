008a97c2c8f16992218e0e1c59a8a39f9b10395e
Kaylee <kayleespindler@gmail.com>
2024-01-28 11:32:45 -0600

    fix

--- jobsdb/models.py
+++ jobsdb/models.py
@@ -8,17 +8,17 @@ class Address(models.Model):
     city = models.CharField(max_length = 50, blank = False)
     state = models.CharField(max_length = 40, blank = True)
 
-class Client(models.Model):
-    email = models.CharField(max_length=254)
-    phone = models.IntegerField()
-    first_name = models.CharField(max_length=50,blank=True, null=True)
-    last_name = models.CharField(max_length=50,blank=True, null=True)
+# class Client(models.Model):
+#     email = models.CharField(max_length=254)
+#     phone = models.IntegerField()
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
 
 class Job(models.Model):
     start_date = models.DateField(auto_now=True)
     active = models.BooleanField(default=False)
     description = models.TextField()
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.CharField(max_length = 40, blank = True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, blank=True, null=True)
 
 # ONJOBCREATE make dummy user
